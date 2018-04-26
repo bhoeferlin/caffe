11b61b495a1f6529ac3462d88a3312c3c2c7013b
Benjamin Höferlin <visionalytics@gmail.com>
2018-04-26 08:49:59 +0200

    Fixed CuDNN always on even in CPU mode

--- src/caffe/layer_factory.cpp
+++ src/caffe/layer_factory.cpp
@@ -52,7 +52,7 @@ shared_ptr<Layer<Dtype> > GetConvolutionLayer(
   if (engine == ConvolutionParameter_Engine_DEFAULT) {
     engine = ConvolutionParameter_Engine_CAFFE;
 #ifdef USE_CUDNN
-    if (!use_dilation) {
+    if (!use_dilation && Caffe::mode() == Caffe::GPU) {
       engine = ConvolutionParameter_Engine_CUDNN;
     }
 #endif
@@ -91,7 +91,7 @@ shared_ptr<Layer<Dtype> > GetDeconvolutionLayer(const LayerParameter& param) {
   if (engine == ConvolutionParameter_Engine_DEFAULT) {
     engine = ConvolutionParameter_Engine_CAFFE;
 #ifdef USE_CUDNN
-    if (!use_dilation) {
+    if (!use_dilation && Caffe::mode() == Caffe::GPU) {
       engine = ConvolutionParameter_Engine_CUDNN;
     }
 #endif
@@ -121,7 +121,10 @@ shared_ptr<Layer<Dtype> > GetPoolingLayer(const LayerParameter& param) {
   if (engine == PoolingParameter_Engine_DEFAULT) {
     engine = PoolingParameter_Engine_CAFFE;
 #ifdef USE_CUDNN
-    engine = PoolingParameter_Engine_CUDNN;
+    if (Caffe::mode() == Caffe::GPU)
+    {
+        engine = PoolingParameter_Engine_CUDNN;
+    }
 #endif
   }
   if (engine == PoolingParameter_Engine_CAFFE) {
@@ -159,7 +162,10 @@ shared_ptr<Layer<Dtype> > GetLRNLayer(const LayerParameter& param) {
 
   if (engine == LRNParameter_Engine_DEFAULT) {
 #ifdef USE_CUDNN
-    engine = LRNParameter_Engine_CUDNN;
+      if (Caffe::mode() == Caffe::GPU)
+      {
+          engine = LRNParameter_Engine_CUDNN;
+      }
 #else
     engine = LRNParameter_Engine_CAFFE;
 #endif
@@ -197,7 +203,10 @@ shared_ptr<Layer<Dtype> > GetReLULayer(const LayerParameter& param) {
   if (engine == ReLUParameter_Engine_DEFAULT) {
     engine = ReLUParameter_Engine_CAFFE;
 #ifdef USE_CUDNN
-    engine = ReLUParameter_Engine_CUDNN;
+    if (Caffe::mode() == Caffe::GPU)
+    {
+        engine = ReLUParameter_Engine_CUDNN;
+    }
 #endif
   }
   if (engine == ReLUParameter_Engine_CAFFE) {
@@ -221,7 +230,10 @@ shared_ptr<Layer<Dtype> > GetSigmoidLayer(const LayerParameter& param) {
   if (engine == SigmoidParameter_Engine_DEFAULT) {
     engine = SigmoidParameter_Engine_CAFFE;
 #ifdef USE_CUDNN
-    engine = SigmoidParameter_Engine_CUDNN;
+    if (Caffe::mode() == Caffe::GPU)
+    {
+        engine = SigmoidParameter_Engine_CUDNN;
+    }
 #endif
   }
   if (engine == SigmoidParameter_Engine_CAFFE) {
@@ -245,7 +257,10 @@ shared_ptr<Layer<Dtype> > GetSoftmaxLayer(const LayerParameter& param) {
   if (engine == SoftmaxParameter_Engine_DEFAULT) {
     engine = SoftmaxParameter_Engine_CAFFE;
 #ifdef USE_CUDNN
-    engine = SoftmaxParameter_Engine_CUDNN;
+    if (Caffe::mode() == Caffe::GPU)
+    {
+        engine = SoftmaxParameter_Engine_CUDNN;
+    }
 #endif
   }
   if (engine == SoftmaxParameter_Engine_CAFFE) {
@@ -269,7 +284,10 @@ shared_ptr<Layer<Dtype> > GetTanHLayer(const LayerParameter& param) {
   if (engine == TanHParameter_Engine_DEFAULT) {
     engine = TanHParameter_Engine_CAFFE;
 #ifdef USE_CUDNN
-    engine = TanHParameter_Engine_CUDNN;
+    if (Caffe::mode() == Caffe::GPU)
+    {
+        engine = TanHParameter_Engine_CUDNN;
+    }
 #endif
   }
   if (engine == TanHParameter_Engine_CAFFE) {
